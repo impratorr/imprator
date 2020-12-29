78eeff8a97b5d24f13398f783f528d8cf1ccc78d
imprator <76734704+impratorr@users.noreply.github.com>
2020-12-29 14:06:12 +0100

    Create imprator.sh

---
+++ imprator.sh
@@ -0,0 +1,255 @@
+#!/bin/bash
+clear
+
+ cd .. 
+ rm -rif usr/etc/motd
+ cd $HOME
+ 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;92m#   this tools created by imprator    # \e[0m\n"           
+printf "\e[1;92m#       add my snapchat                # \e[0m\n"
+printf "\e[1;91m#           @itzz-imprator              # \e[0m\n"
+printf "\e[1;92m#                                      # \e[0m\n"
+printf "\e[1;92m######################################## \e[0m\n"
+sleep 3
+pkg update -y
+clear
+pkg upgrade -y
+sleep 10
+
+echo "       imprator  "
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;90m          imprator ./  \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;96m          imprator ..\  \e[0m\n  "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m          imprator .../ \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m           imprator ....\  \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m           imprator ...../ \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m         imprator ......\ \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m          imprator ......./ \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m         imprator ........\\e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m        imprator........./  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m       imprator  ..........\ "
+printf "\e[1;92m########################################\e[0m\n"
+echo "
+"
+
+termux-setup-storage
+clear
+sleep 2
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m          bost ta pka dawnload abet nano \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 2
+pkg intsall nano -y
+sleep 2 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m          nano ba sarkawtuy dawnload bw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+sleep 2
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m          bosta ta python dawnload abet \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 2
+
+pkg install python -y 
+sleep 1
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m          pythonish ba sarkawtuy dawnload bw  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+clear
+
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m           bosta ta python2 tawaw abet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install python2 -y
+pkg install python3 -y
+sleep 2 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m                   python3 tawaw bu  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              bost ba python2 dawnload bet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m               bosta ruby dawnload bet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install ruby -y
+sleep 2
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m                  rubish tawaw bw \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m            bosta ba openssh dawnload bet\e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install openssl-tool  -y
+sleep 2
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m             openssh dawnload bu tawaw  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              bosta ta curl dawnload bet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 2
+pkg install curl -y
+sleep 2
+clear 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              curlish dawnload bu tawaw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+sleep 2
+clear 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              bost ba git dawnload bet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install git -y 
+sleep 2 
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              git ba  tawawy dawnload bw  \e[0m\n  "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m             bosta ta figlet dawnload abet \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install figlet -y
+sleep 2
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m                figtlet ish tawaw bw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m             bost ta unzip tawaw abet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install unzip -y
+sleep 2
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              unzip dawnload bw tawaw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m             bosta ba zip dawnload bet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+pkg install zip -y
+clear
+sleep 2
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m                 zip dawnload bw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+clear
+
+cd $HOME 
+cd ..
+rm -rif usr/etc/motd
+cd $HOME
+cd ..
+cd usr/etc
+mkdir imprator
+cd shahom
+echo slaw bram am termux to la layn imprator chakrawa bo shty zyatr add snapm bkan  @itzz-imprator >imprator.txt
+cd $HOME
+cd /sdcard
+mkdir imprator
+cd shahowashere 
+echo slaw bram am termux to la layn imprator chakrawa bo shty zyatr add snapm bkan  @itzz-imprator >imprator.txt
+cd $HOME
+pkg install sl -y
+sleep 1
+clear
+printf "\e[1;92m##############################################################\e[0m\n"
+printf "\e[1;91m   xwat lagal bram tawaw bw addm bkan @itzz-imprator snap    \e[0m\n"
+printf "\e[1;92m#############################################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m  xwat lagal bram tawaw bw addm bkan @itzz-imprator snap  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m  xwat lagal bram tawaw bw addm bkan @itzz-imprator snap  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m  xwat lagal bram tawaw bw addm bkan @itzz-imprator snap  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m####################################\e[0m\n"
+printf "\e[1;91m   xwat lagal bram tawaw bw addm bkan @itzz-imprator snap   \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
+
+printf "\e[1;91m  xwat lagal bram tawaw bw addm bkan @itzz-imprator snap \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+clear
+sleep 1
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m   xwat lagal bram tawaw bw addm bkan @itzz-imprator snap  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
+sleep 5
+pkg install tor -y
+cd $HOME
+
+figlet snapchat
+xdg-open https://www.snapchat.com/add/itzz-imprator
+sleep 10
+
+pkg install php -y
+printf "\e[1;94m########################################\e[0m\n"
+printf "\e[1;94m#   this tools G by imprator       # \e[0m\n"           
+printf "\e[1;4m#       add  my   snapchat           # \e[0m\n"
+printf "\e[1;94m#           @itzz-imprator            # \e[0m\n"
+printf "\e[1;94m#                                      # \e[0m\n"
+printf "\e[1;94m######################################## \e[0m\n"
+sleep 3
+figlet good bye 
+printf "\e[1;92m snapchat  @itzz-imprator \e[0m\n"\
